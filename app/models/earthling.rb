a6cbef3da8171c735495e2a6ca73843c48a09222
Tyrone <tyrone.cloma0219@gmail.com>
2022-08-16 16:12:41 -0400

    commit tree c739b28721bf525e1881bffcd48b3cfa642ffb66

--- app/models/earthling.rb
+++ app/models/earthling.rb
@@ -1,3 +1,4 @@
 class Earthling < ActiveRecord::Base
-
+  has_many :visitations
+  
 end
--- app/models/visitation.rb
+++ app/models/visitation.rb
@@ -1,3 +1,9 @@
 class Visitation < ActiveRecord::Base
+  belongs_to :alien
+  belongs_to :earthling
+  
+  def visitations
+    self.visitations
+  end
 
 end
